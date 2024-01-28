247c24cd9d142f30f97cfe554bd09246681d4fba
Matt McMurray <mathieumcmurray@gmail.com>
2024-01-28 12:14:07 -0600

    Read input lines in through BufReader

--- advent_of_code/day1/src/main.rs
+++ advent_of_code/day1/src/main.rs
@@ -1,16 +1,25 @@
 use std::fs::File;
+use std::io::{self, BufRead, Lines, Result};
+use std::path::Path;
 
 const INPUT_FILE_PATH: &str = "input.txt";
 
 fn main() {
-    open_input(&INPUT_FILE_PATH);
+    if let Ok(lines) = read_input_file_lines(INPUT_FILE_PATH) {
+        for line in lines.flatten() {
+            println!("{}", line);
+        }
+    }
 }
 
-fn open_input(path: &str) -> File {
-    let file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", path, why),
-        Ok(file) => file,
-    };
-
-    file
+fn read_input_file_lines<P>(path: P) -> Result<Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    // the `?` syntax at the end of an operation returning a `Result`
+    // is equivalent to a `match` expression where the `Err(err)`
+    // branch expands to an early `return Err(...)`
+    // and the `Ok` branch expands to an `ok` expression
+    let file = File::open(path)?;
+    Ok(io::BufReader::new(file).lines())
 }
