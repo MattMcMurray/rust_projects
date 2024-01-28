dfe09d6229516397566dfeb10ce1d87a8af7163c
Matt McMurray <mathieumcmurray@gmail.com>
2024-01-28 13:54:13 -0600

    Complete part 1 of day 1

--- advent_of_code/day1/src/main.rs
+++ advent_of_code/day1/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Either;
 use std::fs::File;
 use std::io::{self, BufRead, Lines, Result};
 use std::path::Path;
@@ -5,14 +6,24 @@ use std::path::Path;
 const INPUT_FILE_PATH: &str = "input.txt";
 
 fn main() {
-    if let Ok(lines) = read_input_file_lines(INPUT_FILE_PATH) {
+    let mut sum: i32 = 0;
+
+    if let Ok(lines) = read_lines(INPUT_FILE_PATH) {
         for line in lines.flatten() {
-            println!("{}", line);
+            let first = first_digit(&line, false);
+            let last = first_digit(&line, true);
+            let combined = format!("{}{}", first, last);
+            sum += match combined.parse::<i32>() {
+                Ok(val) => val,
+                Err(e) => panic!("Trouble parsing int: {}", e),
+            };
         }
     }
+
+    println!("Final value: {}", sum);
 }
 
-fn read_input_file_lines<P>(path: P) -> Result<Lines<io::BufReader<File>>>
+fn read_lines<P>(path: P) -> Result<Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
 {
@@ -22,4 +33,26 @@ where
     // and the `Ok` branch expands to an `ok` expression
     let file = File::open(path)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+fn first_digit(s: &String, reverse: bool) -> u32 {
+    let mut digit: u32 = 0;
+
+    let eval = if reverse {
+        Either::Left(s.chars().rev())
+    } else {
+        Either::Right(s.chars())
+    };
+
+    for c in eval {
+        if c.is_numeric() {
+            digit = match c.to_digit(10) {
+                Some(digit) => digit,
+                None => panic!(),
+            };
+            break;
+        }
+    }
+
+    digit
 }
