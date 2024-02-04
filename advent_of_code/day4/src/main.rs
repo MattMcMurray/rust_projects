d4025f291ff5dfc0c1c591043b397d84655574c2
Matt McMurray <mathieumcmurray@gmail.com>
2024-02-03 20:11:02 -0600

    [AoC Day4] Complete part 1 successfully

--- advent_of_code/day4/src/main.rs
+++ advent_of_code/day4/src/main.rs
@@ -1,7 +1,48 @@
+use std::io::{self, BufRead, Lines, Result as IOResult};
+use std::fs::File;
+use std::path::Path;
+
 mod scratchcard;
 
 use scratchcard::Scratchcard;
 
+const INPUT_FILE_PATH: &str = "input.txt";
+
 fn main() {
-    println!("Hello, world!");
+    let total = process_input(INPUT_FILE_PATH);
+
+    println!("Total: {}", total);
+}
+
+fn process_input(filepath: &str) -> i32 {
+    let mut total: i32 = 0;
+
+    let mut lines = read_lines(filepath).unwrap();
+
+    while let Some(Ok(line)) = lines.next() {
+        let scratchcard = Scratchcard::new(&line).unwrap();
+
+        total += scratchcard.calculate_score();
+    }
+
+    return total;
+}
+
+fn read_lines<P>(path: P) -> IOResult<Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(path)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process_input() {
+        let r = process_input("test.txt");
+        assert_eq!(r, 13);
+    }
 }
--- advent_of_code/day4/src/scratchcard.rs
+++ advent_of_code/day4/src/scratchcard.rs
@@ -1,22 +1,19 @@
 /**
- * For example:
-
-Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-In the above example, card 1 has five winning numbers (41, 48, 83, 86, and 17) and eight numbers you have (83, 86, 6, 31, 17, 9, 48, and 53). Of the numbers you have, four of them (48, 83, 17, and 86) are winning numbers! That means card 1 is worth 8 points (1 for the first match, then doubled three times for each of the three matches after the first).
-
-Card 2 has two winning numbers (32 and 61), so it is worth 2 points.
-Card 3 has two winning numbers (1 and 21), so it is worth 2 points.
-Card 4 has one winning number (84), so it is worth 1 point.
-Card 5 has no winning numbers, so it is worth no points.
-Card 6 has no winning numbers, so it is worth no points.
-So, in this example, the Elf's pile of scratchcards is worth 13 points.
-
- */
+ * Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+ * Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+ * Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+ * Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+ * Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+ * Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+ * In the above example, card 1 has five winning numbers (41, 48, 83, 86, and 17) and eight numbers you have (83, 86, 6, 31, 17, 9, 48, and 53). Of the numbers you have, four of them (48, 83, 17, and 86) are winning numbers! That means card 1 is worth 8 points (1 for the first match, then doubled three times for each of the three matches after the first).
+ * 
+ * Card 2 has two winning numbers (32 and 61), so it is worth 2 points.
+ * Card 3 has two winning numbers (1 and 21), so it is worth 2 points.
+ * Card 4 has one winning number (84), so it is worth 1 point.
+ * Card 5 has no winning numbers, so it is worth no points.
+ * Card 6 has no winning numbers, so it is worth no points.
+ * So, in this example, the Elf's pile of scratchcards is worth 13 points.
+**/
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Scratchcard {
@@ -51,6 +48,22 @@ impl Scratchcard {
             numbers,
         })
     }
+
+    pub fn calculate_score(&self) -> i32 {
+        let mut num_hits = 0;
+
+        for num in &self.numbers {
+            if self.winners.contains(&num) {
+                num_hits += 1;
+            }
+        }
+
+        if num_hits > 0 {
+            return 1 << num_hits - 1;
+        }
+
+        return 0;
+    }
 }
 
 #[cfg(test)]
@@ -68,5 +81,33 @@ mod tests {
         };
 
         assert_eq!(s, Some(e));
+    }
+
+    #[test]
+    fn test_calculate_score() {
+        let mut input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
+        let mut s = Scratchcard::new(input).unwrap();
+        assert_eq!(s.calculate_score(), 8);
+
+
+        input = "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19";
+        s = Scratchcard::new(input).unwrap();
+        assert_eq!(s.calculate_score(), 2);
+
+        input = "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1";
+        s = Scratchcard::new(input).unwrap();
+        assert_eq!(s.calculate_score(), 2);
+
+        input = "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83";
+        s = Scratchcard::new(input).unwrap();
+        assert_eq!(s.calculate_score(), 1);
+
+        input = "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36";
+        s = Scratchcard::new(input).unwrap();
+        assert_eq!(s.calculate_score(), 0);
+
+        input = "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        s = Scratchcard::new(input).unwrap();
+        assert_eq!(s.calculate_score(), 0);
     }
 }
